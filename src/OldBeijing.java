91673d135ed067522fac248b492b044748911fe8
Kewei Zhang <s4366844@labs.eait.uq.edu.au>
2016-01-13 17:08:13 +1000

    Initial commit

---
+++ src/OldBeijing.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+
+public class OldBeijing {
+
+		private HashMap<Integer, String> node;
+		
+		private OldBeijing(){
+			this.node = new HashMap<Integer, String>();
+			
+		}
+}
